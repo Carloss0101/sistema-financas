ba6c2f910b8a043caac5d3d1b6c53d0e866ab50f
Carloss0101 <contato.carlosedu1@gmail.com>
2026-04-03 14:06:50 -0300

    Criação da classe usuário

---
+++ src/main/java/com/example/dindingo/model/Usuario.java
@@ -0,0 +1,54 @@
+package com.example.dindingo.model;
+
+public class Usuario {
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+
+    public Usuario(String nome, String senha, String email, String cpf) {
+        this.nome = nome;
+        this.senha = senha;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public Usuario() {
+    }
+
+    public boolean validarLogin() {
+        return true;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
