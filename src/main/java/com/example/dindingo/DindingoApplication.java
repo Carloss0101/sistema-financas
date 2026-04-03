0e48c3f78b816ccd521e856c5c7c4716fcaf6297
Carloss0101 <contato.carlosedu1@gmail.com>
2026-04-03 11:53:17 -0300

    first commit

---
+++ src/main/java/com/example/dindingo/DindingoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dindingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DindingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DindingoApplication.class, args);
+	}
+
+}
