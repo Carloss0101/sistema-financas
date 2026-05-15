ec51a5be48e4b26533623ad424a8125999c64461
Carloss0101 <contato.carlosedu1@gmail.com>
2026-05-15 20:39:52 -0300

    sistema de lancamento

---
+++ src/main/java/com/example/dindingo/repository/LancamentoRepository.java
@@ -0,0 +1,14 @@
+package com.example.dindingo.repository;
+
+import com.example.dindingo.model.Usuario;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface LancamentoRepository extends JpaRepository<Usuario, Long> {
+}
