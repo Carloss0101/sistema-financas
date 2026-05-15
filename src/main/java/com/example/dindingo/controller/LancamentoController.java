6223f4e3cccf6795bd47650102b2ecff37c02991
João Caetano Biagio <09jcbl@gmail.com>
2026-05-15 20:40:56 -0300

    fazendo LancamentoController

--- src/main/java/com/example/dindingo/controller/LancamentoController.java
+++ src/main/java/com/example/dindingo/controller/LancamentoController.java
@@ -1,9 +1,45 @@
 package com.example.dindingo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.dindingo.model.Lancamentos;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.dindingo.service.LaunchService;
+
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/lancamentos")
 public class LancamentoController {
+
+    private final LaunchService lancamentoService;
+
+    public LancamentoController(LaunchService lancamentoService) {
+        this.lancamentoService = lancamentoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> salvar(@RequestBody Lancamentos lancamento) {
+        lancamentoService.salvar(lancamento);
+        return ResponseEntity.ok("Lançamento criado com sucesso!");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> editar(@PathVariable int id, @RequestBody Lancamentos lancamento) {
+        lancamento.setId(id);
+        lancamentoService.editar(lancamento);
+        return ResponseEntity.ok("Lançamento atualizado com sucesso!");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletar(@PathVariable int id) {
+        lancamentoService.deletar(id);
+        return ResponseEntity.ok("Lançamento removido com sucesso!");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Lancamentos>> listar() {
+        List<Lancamentos> lista = lancamentoService.listar();
+        return ResponseEntity.ok(lista);
+    }
 }
