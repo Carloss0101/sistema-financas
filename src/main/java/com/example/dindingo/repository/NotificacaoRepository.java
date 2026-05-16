a75e4e719069c22eb1b469a96e88b9508daebb79
Carloss0101 <contato.carlosedu1@gmail.com>
2026-05-15 21:41:44 -0300

    sistema de notificacao

---
+++ src/main/java/com/example/dindingo/repository/NotificacaoRepository.java
@@ -0,0 +1,10 @@
+package com.example.dindingo.repository;
+
+import com.example.dindingo.model.Notificacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotificacaoRepository extends JpaRepository<Notificacao, Long> {
+    List<Notificacao> findByUsuarioId(Long usuarioId);
+}
